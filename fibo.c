7ce57a2af437707f1e6b293074c631c9cbe734af
Guna Shekar <gunashekherproddatoori@gmail.com>
2018-10-09 22:59:45 +0530

    Fibonacci Sequence Code Added

---
+++ fibo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int fibo(int n)
+{
+	if(n==1)
+		return 0;
+	if(n==2)
+		return 1;
+	if(n>=3)
+		return fibo(n-1)+fibo(n-2);
+}
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	for(i=1;i<=n;++i)
+	{
+		printf("%d\n",fibo(i));
+	}
+	return 0;
+}
